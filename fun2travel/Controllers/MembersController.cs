f4ffac59a6fa4b5be86812ec55b9393365cf05b5
Teodor Doroftei <teodor.doroftei@outlook.com>
2018-04-02 16:19:07 +0200

    added memberscontroller, accountregisternewuserVM, loginVM, accountrepository and some changes to login.cshtml

--- fun2travel/Controllers/MembersController.cs
+++ fun2travel/Controllers/MembersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using fun2travel.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,22 @@ namespace fun2travel.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly AccountRepository repository;
+
+        public MembersController(AccountRepository repository)
+        {
+            this.repository = repository;
+
+        }
         // GET: /<controller>/
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+
+        //Här ska routen och action till inloggningsida för admin och andra users vara
+
+        //sätt [Authorize] på de sidor som bara skall nås när man är inloggad.
     }
 }
---
+++ fun2travel/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fun2travel.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
