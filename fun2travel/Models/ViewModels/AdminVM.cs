ae17c005a19814f61488aae04ffbd728797962ab
Teodor Doroftei <teodor.doroftei@outlook.com>
2018-04-03 14:13:20 +0200

    städat lite i koden, lagt till medelande till användare efter man bokat

--- fun2travel/Models/ViewModels/AdminVM.cs
+++ fun2travel/Models/ViewModels/AdminVM.cs
@@ -10,23 +10,6 @@ namespace fun2travel.Models.ViewModels
     {
         public string UserName { get; set; }
 
-        //public int BookingId { get; set; }
-        //public DateTime TimeStamp { get; set; }
-        //public DateTime DateFrom { get; set; }
-        //public DateTime DateTo { get; set; }
-        //public string HotelName { get; set; }
-        //public int NoPplForHotel { get; set; }
-        //public int ActivityId { get; set; }
-        //public int NoPplForActivity { get; set; }
-        //public bool RentEquipment { get; set; }
-        //public bool Transport { get; set; }
-        //public string FirstName { get; set; }
-        //public string LastName { get; set; }
-        //public string BookingEmail { get; set; }
-        //public string BookingPhone { get; set; }
-        //public double TotalCost { get; set; }
-        //public int TotalNoNights { get; set; }
-
         public List<Booking> Bookings { get; set; }
 
     }
